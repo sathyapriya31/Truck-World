191687de3cda9a26da0318727e4f4b61613f482c
ranjith <revaret@gmail.com>
2015-08-10 17:58:47 +0530

    task bar colour changed

--- src/com/spritle/truck_apps/MainActivity.java
+++ src/com/spritle/truck_apps/MainActivity.java
@@ -1,7 +1,13 @@
 package com.spritle.truck_apps;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBar;
+import android.view.View;
+import android.view.ViewParent;
+import android.widget.TextView;
 
 public class MainActivity extends FragmentActivity {
 
@@ -9,5 +15,9 @@ public class MainActivity extends FragmentActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		android.app.ActionBar bar = getActionBar();
+		bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#607d8b")));
+		
 	}
 }
